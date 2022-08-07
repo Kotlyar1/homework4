2ad76532e29b9f5b996c96e245ca8005dc016adb
Александр Котляров <bony17@bk.ru>
2022-08-07 10:44:31 +0300

    решил 27 задачу

---
+++ task27/Program.cs
@@ -0,0 +1,35 @@
+﻿try
+{
+    Console.Write("Введите целое число: ");
+    Int numbers = int.Parse(Console.ReadLine());
+
+    int SumNumbersInNumber(int number)
+    {
+        int sum = 0;
+        int ResNum = GetCountNumber(number);
+        for (int i = 0; i < ResNum; i++)
+        {
+            sum = sum + (number % 10);
+            number = (number / 10);
+        }
+        return sum;
+    }
+
+    int GetCountNumber(int num)
+    {
+        int count = 0;
+        while (num != 0)
+        {
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+
+    int sumResult = SumNumbersInNumbers(numbers);
+    Console.WriteLine("Сумма цифр числа {0} = {1}", numbers, sumResult);
+}
+catch
+{
+    Console.WriteLine("Некорректный ввод!");
+}
